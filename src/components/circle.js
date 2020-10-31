333746d47aa0a0678f928f5a7cd1490ab97f5038
abhisheksonkar20 <sonkar.abhishek45@gmail.com>
2020-10-31 15:19:45 +0530

    Updated Shape & Circle

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,11 @@
-// import using require
-
-// declare class
-
-
-// export class using module.exports
+const Shape = require('./shape');
+class Circle {
+    constructor(){
+        super();
+    }
+    calculateArea() {
+        console.log("Area of Shape class with color" + this.color + " has been Overriden");
+        return 3.14 * 2 * 2;
+    }
+}
+module.exports = Circle;
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,12 @@
-// declare class
-
-// export class using module.exports
+class Shape {
+    constructor() {
+        this.color = "green";
+    }
+    drawShape() {   
+        console.log("Shape is drawn");
+    }
+    claculateArea() {
+        console.log ("Area of shape is calculated");
+    }
+}
+module.exports = Shape;
